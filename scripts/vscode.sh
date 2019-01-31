981c95ac76f179a330658eb17d50e1ce4046d25b
Morten Olsen <morte@olsen.pro>
2019-01-31 14:25:08 +0100

    Fixed VSCode installer

--- scripts/vscode.sh
+++ scripts/vscode.sh
@@ -14,8 +14,14 @@ export EDITOR="code --wait"
 
 function do_install {
   mkdir -p "$PACKAGE_DIR/download/code-portable-data"
-  setupZip "https://go.microsoft.com/fwlink/?LinkID=620882"
+  INITPATH="$PWD"
+  mkdir -p "$PACKAGE_DIR/download/code-portable-data"
+  curl https://go.microsoft.com/fwlink/?LinkID=620882 -L -o "$PACKAGE_DIR/download/vscode.zip"
+  cd $PACKAGE_DIR/download
+  unzip "$PACKAGE_DIR/download/vscode.zip"
+  rm "$PACKAGE_DIR/download/vscode.zip"
   xattr -dr com.apple.quarantine "$PACKAGE_DIR/download/Visual Studio Code.app"
+  cd $INITPATH
   mkdir -p "$PACKAGE_DIR/bin"
   echo '
   #!/usr/bin/env bash
