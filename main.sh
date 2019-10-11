34684b97914edceaebe84573e1938b667d0b5a68
Morten olsen <morten@olsen.pro>
2019-10-11 14:15:10 +0200

    cleanup

--- main.sh
+++ main.sh
@@ -8,12 +8,6 @@ if [[ -n "$SSH_CONNECTION" ]] ;then
   export PINENTRY_USER_DATA="USE_CURSES=1"
 fi
 
-
-# added by travis gem
-[ -f /home/morten/.travis/travis.sh ] && source /home/morten/.travis/travis.sh
-
-test -e "${HOME}/.iterm2_shell_integration.zsh" && source "${HOME}/.iterm2_shell_integration.zsh"
-
 if hash thefuck 2>/dev/null; then
   eval $(thefuck --alias)
 fi
