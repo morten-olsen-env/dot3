80d7a857225b7f1b7db0a1745f5b904a3d0ca916
Morten Olsen <morten@olsen.pro>
2019-10-13 21:30:43 +0200

    added nvim for Cataline

--- scripts/nvim.sh
+++ scripts/nvim.sh
@@ -2,16 +2,19 @@ function get_env {
   echo '
 export VIM_BASE_PATH="'"$PACKAGE_DIR"'"
 export VIM_PLUGIN_PATH="$VIM_BASE_PATH/plugged"
-export VIM_EXECUTABLE="nvim"
+export VIM_EXECUTABLE="$VIM_BASE_PATH/download/nvim-osx64/bin/nvim"
 alias vim="$VIM_EXECUTABLE -u $VIM_BASE_PATH/init.vim -N"
 export EDITOR="$VIM_EXECUTABLE -u $VIM_BASE_PATH/init.vim -N"
   '
 }
 
+# https://github.com/neovim/neovim/releases/download/nightly/nvim-macos.tar.gz
+
 function do_install {
   add_config init.vim init.vim 
   curl -fLo $PACKAGE_DIR/autoload/plug.vim --create-dirs \
     "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
+  setupZip https://github.com/neovim/neovim/releases/download/nightly/nvim-macos.tar.gz
 }
 
 function is_installed {
